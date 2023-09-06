167acfb2aac61efdeac22b7c40ef24b17c62fe58
Tulika-Paul99 <paultulika216@gmail.com>
2023-09-06 12:16:06 +0530

    first commit

---
+++ src/pages/homepage.js
@@ -0,0 +1,24 @@
+import { useContext } from 'react';
+import{AuthContext} from "../context/authContext";
+
+function Homepage() {
+    const {user,logout} = useContext(AuthContext);
+
+    return(
+        <>
+            <h1>This is the homepage</h1>
+            { user ?
+                <>
+                <h2>{user.email} is logged in</h2>
+                </>
+            :
+
+                <>
+                <p>There is no user data</p>
+                </>
+            }
+        </>
+    )
+}
+
+export default Homepage;
